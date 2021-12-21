2ee2969a70e44eeccde26ac3687c0906088aa4ba
LVViet160998 <Lonzchoia12>
2021-12-21 13:30:58 +0700

    -- Clean Code

--- Model/Cache/Type.php
+++ Model/Cache/Type.php
@@ -1,7 +1,22 @@
 <?php
-/*
- * Author: Aleš Cankar (ales.cankar@optiweb.com)
- * Copyright (c) 2021 Optiweb (https://www.optiweb.com)
+/**
+ * Mageplaza
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Mageplaza.com license that is
+ * available through the world-wide-web at this URL:
+ * https://www.mageplaza.com/LICENSE.txt
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category    Mageplaza
+ * @package     Mageplaza_InstagramFeed
+ * @copyright   Copyright (c) Mageplaza (https://www.mageplaza.com/)
+ * @license     https://www.mageplaza.com/LICENSE.txt
  */
 
 namespace Mageplaza\InstagramFeed\Model\Cache;
